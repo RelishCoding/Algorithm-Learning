d4dbc363a126d62971c4114cf2ad05a822c300a7
qzh <2806894439@qq.com>
2023-03-26 22:12:30 +0800

    T455分发饼干

---
+++ 贪心/src/T455.java
@@ -0,0 +1,34 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class T455 {
+    // 思路1：优先考虑饼干，小饼干先喂饱小胃口
+    /*public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int count=0;
+        int index=0;
+        for (int i=0;i<s.length;i++){
+            if (index<g.length && s[i]>=g[index]){
+                count++;
+                index++;
+            }
+        }
+        return count;
+    }*/
+
+    // 思路2：优先考虑胃口，先喂饱大胃口
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int count=0;
+        int index=s.length-1;
+        for (int i=g.length-1;i>=0;i--){
+            if (index>=0 && s[index]>=g[i]){
+                count++;
+                index--;
+            }
+        }
+        return count;
+    }
+}
