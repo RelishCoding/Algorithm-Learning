633ef16426d3ca0404ac27b76e8cc0cd3f957613
qzh <2806894439@qq.com>
2023-05-30 22:40:56 +0800

    T63不同路径2

---
+++ DynamicProgramming/src/T63.java
@@ -0,0 +1,28 @@
+public class T63 {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m=obstacleGrid.length;
+        int n=obstacleGrid[0].length;
+
+        //如果在起点或终点出现了障碍，直接返回0
+        if (obstacleGrid[m-1][n-1]==1 || obstacleGrid[0][0]==1)
+            return 0;
+
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m && obstacleGrid[i][0] == 0; i++) {
+            dp[i][0]=1;
+        }
+        for (int j = 0; j < n && obstacleGrid[0][j] == 0; j++) {
+            dp[0][j]=1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j]==0)
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                else
+                    dp[i][j]=0;//也可写continue，因为数组默认初始化为0
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
---
+++ 链表/src/T203.java
@@ -0,0 +1,49 @@
+public class T203 {
+    /*public ListNode removeElements(ListNode head, int val) {
+        // 删除头结点
+        // 注意这里要用while而不能用if
+        // 因为可能连着几个结点的值都为val
+        // 循环条件写head！=null是因为：
+        // 1.可能传进来的是空链表
+        // 2.可能链表中全部结点的值都为val，删除完后head为null，再访问head.val会报错
+        while ( head!=null && head.val==val){
+            head=head.next;
+        }
+
+        // 删除非头结点
+        ListNode cur=head;
+        while (cur!=null && cur.next!=null){
+            if (cur.next.val==val){
+                cur.next=cur.next.next;
+            }else {
+                cur=cur.next;
+            }
+        }
+        return head;
+    }*/
+
+    public ListNode removeElements(ListNode head, int val) {
+        if (head==null)
+            return head;
+        // 设置一个虚拟头结点
+        ListNode dummyHead = new ListNode();
+        // 将虚拟头结点指向head，这样方面后面做删除操作
+        dummyHead.next=head;
+        ListNode cur = dummyHead;
+        while (cur.next!=null){
+            if (cur.next.val==val)
+                cur.next=cur.next.next;
+            else
+                cur=cur.next;
+        }
+        return dummyHead.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
