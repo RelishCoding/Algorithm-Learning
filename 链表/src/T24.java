c29d4e4f04ae1fe2b69ab2e7ae47022c5b383583
qzh <2806894439@qq.com>
2023-09-11 22:13:26 +0800

    T24两两交换链表中的节点

--- 链表/src/T24.java
+++ 链表/src/T24.java
@@ -1,5 +1,6 @@
 public class T24 {
-    public ListNode swapPairs(ListNode head) {
+    //非递归版本
+    /*public ListNode swapPairs(ListNode head) {
         // 设置一个虚拟头结点
         ListNode dummyHead = new ListNode();
         // 将虚拟头结点指向head，这样方面后面做删除操作
@@ -18,6 +19,24 @@ public class T24 {
             cur = cur.next.next;
         }
         return dummyHead.next;
+    }*/
+
+    //递归版本
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null){
+            return head;
+        }
+
+        // 获取当前节点的下一个节点
+        ListNode nextNode = head.next;
+        // 进行递归
+        ListNode newNode = swapPairs(nextNode.next);
+
+        // 交换节点
+        nextNode.next = head;
+        head.next = newNode;
+
+        return nextNode;
     }
 
     private class ListNode{
