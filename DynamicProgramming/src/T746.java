2f6546cae7edfd800c586e3a5d7ab571bcce6dc4
qzh <2806894439@qq.com>
2025-03-22 10:00:16 +0800

    优化格式

--- DynamicProgramming/src/T746.java
+++ DynamicProgramming/src/T746.java
@@ -1,25 +1,28 @@
+/**
+ * Leetcode 746 使用最小花费爬楼梯
+ */
 public class T746 {
-    //非压缩版本
-    /*public int minCostClimbingStairs(int[] cost) {
-        int[] dp=new int[cost.length+1];
-        dp[0]=0;
-        dp[1]=0;
-        for (int i=2;i<=cost.length;i++){
-            //dp[i]=Integer.min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2]);
-            dp[i]=Math.min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2]);
+    // 非压缩版本
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length + 1];
+        dp[0] = 0;
+        dp[1] = 0;
+        for (int i = 2; i <= cost.length; i++) {
+            // dp[i]=Integer.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
         }
         return dp[cost.length];
-    }*/
+    }
 
-    //压缩版本
-    public int minCostClimbingStairs(int[] cost) {
-        int dp0=0;
-        int dp1=0;
+    // 压缩版本
+    public int minCostClimbingStairs2(int[] cost) {
+        int dp0 = 0;
+        int dp1 = 0;
         int dpi;
-        for (int i=2;i<=cost.length;i++){
-            dpi=Math.min(dp1+cost[i-1],dp0+cost[i-2]);
-            dp0=dp1;
-            dp1=dpi;
+        for (int i = 2; i <= cost.length; i++) {
+            dpi = Math.min(dp1 + cost[i - 1], dp0 + cost[i - 2]);
+            dp0 = dp1;
+            dp1 = dpi;
         }
         return dp1;
     }
